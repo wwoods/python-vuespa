776935f94b277a40905b27ae9ba35045b415f8b5
Walt Woods <waltw@galois.com>
2020-05-28 07:44:07 -0700

    v0.3.0 Fixed example shim for compiling

--- test/1_basic_app_vue/src/shims-vuespa.d.ts
+++ test/1_basic_app_vue/src/shims-vuespa.d.ts
@@ -2,9 +2,9 @@ import Vue from 'vue';
 declare module 'vue' {
   export default interface Vue {
     $vuespa: {
-      call: (fn: string, ...args: any[]),
+      call: (fn: string, ...args: any[]) => Promise<any>,
       httpHandler: (cb: {(url: string): void}, fns: {[name: string]: {(args: any): void}}) => {(): void},
-      update: (name: string, fn: string, ...args: any[]),
+      update: (name: string, fn: string, ...args: any[]) => Promise<void>,
     };
   }
 }
